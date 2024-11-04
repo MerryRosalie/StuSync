d834551854464c860b9328d27b875739ad27f581
Merry Rosalie <merryrosalie001@gmail.com>
2024-11-04 17:09:51 +1100

    chore: setup react native app with expo

---
+++ app/_layout.js
@@ -0,0 +1,29 @@
+// import { Tabs } from "expo-router";
+// import Ionicons from "@expo/vector-icons/Ionicons";
+
+// export default function RootLayout() {
+//   return (
+//     <Tabs>
+//       <Tabs.Screen
+//         name="notes"
+//         options={{
+//           title: "Notes",
+//           headerShown: false,
+//           tabBarIcon: ({ size, color }) => (
+//             <Ionicons name="document-text" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="settings"
+//         options={{
+//           title: "Settings",
+//           tabBarIcon: ({ size, color }) => (
+//             <Ionicons name="settings" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen name="index" options={{ href: null }} />
+//     </Tabs>
+//   );
+// }
