fee9f0ea3db9327f3eeff6643dc6584acabda5a8
Urja Arora <urjaarora5@gmail.com>
2024-11-13 16:16:48 +0530

    :recycle: dark mode login

--- app/auth/login.js
+++ app/auth/login.js
@@ -1,28 +1,28 @@
 import { Text, TextInput, TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
-import LoginImage from "../../assets/login/welcome-back.png";
+import LoginImage from "../../assets/auth/sign-in.png";
 
 export default function Page() {
   return (
-    <SafeAreaView className="flex justify-start items-center bg-purple-secondary h-full">
+    <SafeAreaView className="flex justify-start items-center bg-purple-secondary dark:bg-dark-purple-secondary h-full">
       <Image
         source={LoginImage}
         className="px-12 mt-16 mb-5"
       />
-      <View className="bg-white w-full h-full px-5 py-12 rounded-t-[30px]">
+      <View className="bg-white dark:bg-dark-background w-full h-full px-5 py-12 rounded-t-[30px]">
         <Text className="font-inter-bold text-3xl text-purple-default dark:text-dark-purple-default self-start mb-8">
           Welcome Back!
         </Text>
 
-        <Text className="font-inter-medium text-lg mb-2">Email</Text>
+        <Text className="font-inter-medium text-lg mb-2 dark:text-dark-text-default">Email</Text>
         <TextInput
           placeholder="Enter your email..."
           className="w-full p-4 rounded-xl bg-white dark:bg-dark-background border border-gray-200 dark:border-gray-700"
           placeholderTextColor="#9CA3AF"
         />
 
-        <Text className="font-inter-medium text-lg my-2">Password</Text>
+        <Text className="font-inter-medium text-lg my-2 dark:text-dark-text-default">Password</Text>
         <TextInput
           placeholder="Enter your password..."
           secureTextEntry
@@ -31,8 +31,8 @@ export default function Page() {
         />
 
         <View className="flex-row justify-center items-center mt-4">
-          <Text className="font-inter-regular">Don't have an account? </Text>
-          <Link href="/auth/signup" className="font-inter-medium text-purple-default">
+          <Text className="font-inter-regular dark:text-dark-text-default">Don't have an account? </Text>
+          <Link href="/auth/register" className="font-inter-medium text-purple-default">
             Sign Up
           </Link>
         </View>
